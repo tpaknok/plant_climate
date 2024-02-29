99a40fbfe6ee66371ff6334345593943575f5f5f
tpaknok <tpaknok@gmail.com>
2024-02-29 16:04:54 -0500

    written in SQL language instead of dplyr. Updated filters

--- Code/02_SQL.R
+++ Code/02_SQL.R
@@ -1,17 +1,27 @@
 library(inborutils)
 library(dplyr)
-
+library(DBI)
 #can replace Magnoliopsidae with liliopsida
 csv_to_sqlite(csv_file = "GBIF_plants.csv", "GBIF_plants.sqlite", "Plants", delim="\t",pre_process_size = 1000, chunk_size = 50000, show_progress_bar = T)
 
 con <- DBI::dbConnect(RSQLite::SQLite(), "C:/Users/pakno/OneDrive - University of Toronto/GLONAF/GBIF_plants.sqlite")
 plants <- tbl(con, "Plants")
 
-subset_plants <- plants %>% 
-  group_by(species) %>% 
-  filter(coordinateUncertaintyInMeters <= 1000) %>% 
-  select(decimalLongitude,decimalLatitude,genus,species,issue,basisOfRecord)
+### SQL code - need to fetch it here before running some data cleaning function
+subset_plants <- dbGetQuery(con,
+                             'SELECT decimalLongitude,decimalLatitude,genus,species,issue,basisOfRecord
+                             FROM Plants
+                             WHERE coordinateUncertaintyInMeters <= 1000
+                             AND (basisOfRecord = "HUMAN_OBSERVATION" 
+                             OR basisOfRecord = "PRESERVED_SPECIMEN"
+                             OR basisOfRecord = "OCCURRENCE"
+                             OR basisOfRecord = "MATERIAL_SAMPLE"
+                             OR basisOfRecord = "MATERIAL_CITATION")')
+
+### dplyr version (just a part of it) ###
+#subset_plants1 <- plants %>% 
+  #filter(coordinateUncertaintyInMeters <= 1000) %>% 
+  #select(decimalLongitude,decimalLatitude,genus,species,issue,basisOfRecord)
 
 write.csv(subset_plants,"plants.csv")
 
-plants <- read.csv("plants.csv")
--- Code/03_clean_data.R
+++ Code/03_clean_data.R
@@ -2,8 +2,6 @@
 library(CoordinateCleaner)
 plants <- read.csv("C:/Users/pakno/OneDrive - University of Toronto/GLONAF/plants.csv")
 
-plants <- plants[c(-17761060,-26905671,-37021050),]
-plants <- plants[plants$basisOfRecord == "HUMAN_OBSERVATION" | plants$basisOfRecord == "PRESERVED_SPECIMEN" | plants$basisOfRecord == "OCCURRENCE" | plants$basisOfRecord == "MATERIAL_CITATION" | plants$basisOfRecord == "MATERIAL_SAMPLE",]
 plants_db_cleaned <- clean_coordinates(plants,
                                        lon="decimalLongitude",
                                        lat="decimalLatitude",
